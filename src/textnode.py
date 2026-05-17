bf7cb8ae774e7ec7e0b507c73b21e10c17ea36ec
Julian Aladio <jmaladio@gmail.com>
2026-05-17 19:08:25 -0300

    feat: add base structure

---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain_text"
+    BOLD_TEXT = "bold_text"
+    ITALIC_TEXT = "italic_text"
+    CODE_TEXT = "code_text"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url or None
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
