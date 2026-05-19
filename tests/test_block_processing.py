c72c0564be6fd9ec7a5481a8feeba4cf17dd3e88
Julian Aladio <jmaladio@gmail.com>
2026-05-19 21:53:34 +0000

    feat: add markdown to html nodes converter

--- tests/test_block_processing.py
+++ tests/test_block_processing.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_processing import markdown_to_blocks, block_to_block_type, BlockType
+from block_processing import markdown_to_blocks, block_to_block_type, BlockType, markdown_to_html_node
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -46,3 +46,34 @@ python\ncode\n
         self.assertEqual(block_to_block_type("1. list\n3. list 2"), BlockType.PARAGRAPH)
         # code block with missing closing fence should fail
         self.assertEqual(block_to_block_type("```\ncode\n"), BlockType.PARAGRAPH)
+
+class TestBlockToHtmlNode(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """```
+This is text that _should_ remain
+the **same** even with inline stuff
+```"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
